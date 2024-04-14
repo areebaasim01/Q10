e7492d8cc29dddf1337273862a6084689e6a2aa0
areebaasim01 <161856617+areebaasim01@users.noreply.github.com>
2024-04-14 15:21:15 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,20 @@
+// 10.	Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+// If you don’t have anything specific to write because your programs are too simple at this point, 
+// just add your name and the current date at the top of each program file. 
+// Then write one sentence describing what the program does.
+
+
+let a: number = 26
+let b: number = 18
+
+// Addition
+console.log(`${a} + ${b} =  ${a + b}`);
+// subraction
+console.log(`${a} - ${b} =  ${a - b}`);
+// multiplication
+console.log(`${a} * ${b} =  ${a * b}`);
+// division
+console.log(`${a} / ${b} =  ${a / b}`);
+
+
+
